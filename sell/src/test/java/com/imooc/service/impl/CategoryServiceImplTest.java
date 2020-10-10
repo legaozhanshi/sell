3f0ab9f5f1e6512b967f331b941e30e84f6dfe7e
E00281 <jinhao.liang@cansemitech.com>
2020-10-10 17:33:08 +0800

    10/10 update

--- sell/src/test/java/com/imooc/service/impl/CategoryServiceImplTest.java
+++ sell/src/test/java/com/imooc/service/impl/CategoryServiceImplTest.java
@@ -20,6 +20,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -41,22 +42,27 @@ public class CategoryServiceImplTest {
     private CategoryServiceImpl categoryService;
 
     @Test
-    public void findOne() {
+    public void findOne() throws Exception{
         ProductCategory productCategory = categoryService.findOne(1);
         Assert.assertEquals(new Integer(1), productCategory.getCategoryId());
     }
 
     @Test
-    public void findAll() {
+    public void findAll() throws Exception{
         List<ProductCategory> productCategoryList = categoryService.findAll();
         Assert.assertNotEquals(0,productCategoryList.size());
     }
 
     @Test
-    public void findByCategoryTypeIn() {
+    public void findByCategoryTypeIn() throws Exception{
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(Arrays.asList(1,2,3,4));
+        Assert.assertNotEquals(0, productCategoryList.size());
     }
 
     @Test
-    public void save() {
+    public void save() throws Exception{
+        ProductCategory productCategory = new ProductCategory("男生专享", 10);
+        ProductCategory result = categoryService.save(productCategory);
+        Assert.assertNotNull(result);
     }
 }
